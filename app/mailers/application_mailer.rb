13c8db442efd5705daaf99440bd4f4a64a020bd7
Jerry Luk <jerryluk@gmail.com>
2015-04-19 23:30:18 -0700

    Use Gmail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   include Roadie::Rails::Automatic
 
   # default from: "info@lizaandjerry.com"
-  default from: "lizaandjerry@gmail.com"
+  default from: "Liza and Jerry <lizaandjerry@gmail.com>"
   layout 'mailer'
 end
