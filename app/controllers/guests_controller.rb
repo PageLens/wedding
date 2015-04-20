f0001bcc2f49641f9557bc90b33cdaae45a171aa
Jerry Luk <jerryluk@gmail.com>
2015-04-20 12:18:43 -0700

    order

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,6 +2,6 @@ class GuestsController < ApplicationController
   http_basic_authenticate_with name: "admin", password: Rails.application.secrets.auth_password
 
   def index
-    @guests = Guest.all
+    @guests = Guest.order(:reservation_id).all
   end
 end
