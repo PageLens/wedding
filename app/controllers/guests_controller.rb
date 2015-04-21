b4b6db112be393bcde1e42c35923379a58c35696
Jerry Luk <jerryluk@gmail.com>
2015-04-21 14:48:24 -0700

    CSV export

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,17 @@
+require 'csv'
+
 class GuestsController < ApplicationController
   http_basic_authenticate_with name: "admin", password: Rails.application.secrets.auth_password
 
   def index
     @guests = Guest.order(:reservation_id).all
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"guests.csv\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
+    end
   end
 end
